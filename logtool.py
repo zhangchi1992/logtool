60d78f4dece6a6e313bf33ac893cecb7fcbe047d
zhangchi <369297171@qq.com>
2015-08-21 11:23:33 +0800

    filename

--- logtool.py
+++ logtool.py
@@ -6,6 +6,7 @@ import getopt
 import paramiko
 import re
 import os
+import string
 from datetime import datetime
 from datetime import timedelta
 
@@ -15,25 +16,25 @@ LOG_LEVELS = ['DEBUG','INFO','WARN','ERROR','FATAL']
 
 #配置模块信息
 #keystone
-KEYSTONE_IP = ['10.95.200.179',]
+KEYSTONE_IP = ['10.95.200.179']
 KEYSTONE_USER = 'root'
 KEYSTONE_PASSWD = 'root'
 KEYSTONE_MANAGE = '/var/log/keystone/keystone-manage.log'
 KEYSTONE_MAIN = '/var/log/keystone/main.log'
 #glance
-GLANCE_IP = ['10.95.200.179',]
+GLANCE_IP = ['10.95.200.179']
 GLANCE_USER = 'root'
 GLANCE_PASSWD = 'root'
 GLANCE_API = '/var/log/glance/glance-api.log'
 GLANCE_REGISTRY = '/var/log/glance/glance-registry.log'
 #cinder
-CINDER_IP = ['10.95.200.179',]
+CINDER_IP = ['10.95.200.179']
 CINDER_USER = 'root'
 CINDER_PASSWD = 'root'
 CINDER_API = '/var/log/cinder/cinder-api.log'
 CINDER_SCHEDULER = '/var/log/cinder/cinder-scheduler.log'
 #nova
-NOVA_IP = ['10.95.200.179','10.95.20.66',]
+NOVA_IP = ['10.95.200.179','10.95.20.66']
 NOVA_USER = 'root'
 NOVA_PASSWD = 'root'
 NOVA_API = '/var/log/nova/nova-api.log'
@@ -41,7 +42,7 @@ NOVA_CONDUCTOR = '/var/log/nova/nova-conductor.log'
 NOVA_MANAGE = '/var/log/nova/nova-manage.log'
 NOVA_SCHEDULER = '/var/log/nova/nova-scheduler.log'
 #nova-compute
-NOVA_COMPUTE_IP = ['10.95.20.66',]
+NOVA_COMPUTE_IP = ['10.95.20.66']
 NOVA_COMPUTE_USER = 'root'
 NOVA_COMPUTE_PASSWD = 'root'
 NOVA_COMPUTE = '/var/log/nova/nova-compute.log'
@@ -57,7 +58,6 @@ start_opt = False
 end_opt = False
 l_opt = False
 server_opt = False
-f_opt = False
 
 #帮助信息
 def usage():  
@@ -66,7 +66,6 @@ def usage():
             -t                  time point              " '2015-08-14 18:00:00'"
             -a                  time around             "-a 10h"  "-a +10m" "-a -10d"
             -l                  level                   "-level warn" "-leval warn:error"
-            -f                  filepath                "-f /home/jack/log"
             --start             start time              "--start '2015-08-14 18:00:00' "
             --end               end time                "--end '2015-08-14 18:00:00' "
             --server            server ip               "--server 10.95.200.179"
@@ -81,15 +80,15 @@ def parse_datetime(arg):
         usage()
         sys.exit(1)
 
-
 #获取命令行
 try:
-    opts,args = getopt.getopt(sys.argv[1:],"m:t:a:l:f:h",["help","start=","server=","end="])
+    opts,args = getopt.getopt(sys.argv[1:],"m:t:a:l:h",["help","start=","server=","end="])
 except getopt.GetoptError,err:
     print str(err)
     usage()
     sys.exit(1)
-
+#处理文件名称
+filename = ''
 #解析命令行参数    
 for o,a in opts:
     if o in ("-h","--help"):
@@ -98,6 +97,7 @@ for o,a in opts:
     if o in ("-m"):
         m_opt = True
         module_name = a.upper().replace('-','_')
+        filename = filename + 'm:' + a +'%'
         if a == "nova-compute":
             module = module_name
         else:
@@ -105,24 +105,27 @@ for o,a in opts:
     if o in ("-t"):
         t_opt = True
         time_point = parse_datetime(a)
+        filename = filename + 't:' + a.replace(' ',',') + '%'
     if o in ("-a"):
         a_opt = True
-        time_around = a
+        time_around = a 
+        filename = filename + 'a:' + a + '%'
     if o in ("--server"):
         s_opt = True
         server_ip = a
+        filename = filename + 'server:' + a + '%'
     if o in ("--start"):
         start_opt = True
         start_time = parse_datetime(a)
+        filename = filename + 'start:' + a.replace(' ',',') + '%'
     if o in ("--end"):
         end_opt = True
         end_time = parse_datetime(a)
+        filename = filename + 'end:' + a.replace(' ',',') + '%'
     if o in ("-l"):
         l_opt = True
         levels = a.split(':')
-    if o in ("-f"):
-        f_opt = True
-        filepath = a
+        filename = filename + 'l:' + a + '%'
 
 #检查命令行格式
 if not m_opt:
@@ -200,13 +203,12 @@ if not l_opt:
 #处理 -s 参数
 if not server_opt:
     server_ip = eval(module + '_IP')
-#处理 -f 参数
-if f_opt:
-    if not os.path.exists(filepath):
-        os.makedirs(filepath)
-    filename = module_name.lower() + '.' + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    log_path = os.path.join(filepath,filename)
-    os.mknod(log_path)
+
+#配置文件路径
+filepath = os.getcwd()
+filename = filename + 'time:' + datetime.now().strftime('%Y-%m-%d,%H:%M:%S')
+log_path = os.path.join(filepath,filename)
+os.mknod(log_path)
 
 #配置时间格式
 start_time = parse_datetime(start_time)
@@ -216,7 +218,8 @@ end_time = parse_datetime(end_time)
 user = eval(module + '_USER')
 passwd = eval(module + '_PASSWD')
 log_dir = eval(module_name)
-command = 'cat '+log_dir
+command = 'cat ' + log_dir
+cmd = 'vim ' + log_path
 
 #SSH远程连接
 def ssh_connect(ip, user, passwd, command):
@@ -231,46 +234,33 @@ def ssh_connect(ip, user, passwd, command):
 
 #匹配log文件
 def print_match_logs(log, start_time, end_time, levels, ip):
-    if f_opt:
-        f = open(log_path,'a')
-        string = "["+ip+"]"+"\n"
-        f.write(string)
-        print string
-        for line in log:
-            m = re.match('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\.\d+\s\d+\s(\w+)', line)
-            if m:
-                timestr = m.group(1)
-                levelstr = m.group(2)
-                try:
-                    cur_time = parse_datetime(timestr)
-                    if cur_time >= start_time and cur_time <= end_time and levelstr in levels:
-                        print line
-                        f.write(line)
-                except ValueError:
-                    pass
-        f.write("\n")
-        f.close()
-    else:
-        for line in log:
-            m = re.match('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\.\d+\s\d+\s(\w+)', line)
-            if m:
-                timestr = m.group(1)
-                levelstr = m.group(2)
-                try:
-                    cur_time = parse_datetime(timestr)
-                    if cur_time >= start_time and cur_time <= end_time and levelstr in levels:
-                        print line
-                except ValueError:
-                    pass
+    f = open(log_path,'a')
+    string = "["+ip+"]"+"\n"
+    f.write(string)
+    for line in log:
+        m = re.match('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\.\d+\s\d+\s(\w+)', line)
+        if m:
+            timestr = m.group(1)
+            levelstr = m.group(2)
+            try:
+                cur_time = parse_datetime(timestr)
+                if cur_time >= start_time and cur_time <= end_time and levelstr in levels:
+                    f.write(line)
+            except ValueError:
+                pass
+    f.write("\n")
+    f.close()
+
 
 #判断是否设定服务IP地址
 if server_opt:
     log = ssh_connect(server_ip,user,passwd,command)
     print_match_logs(log, start_time, end_time, levels, server_ip)
+    os.system(cmd)
 else:
     for ip in server_ip:
         log = ssh_connect(ip,user,passwd,command)
         print_match_logs(log, start_time, end_time, levels, ip)
-
+    os.system(cmd)
 
     
